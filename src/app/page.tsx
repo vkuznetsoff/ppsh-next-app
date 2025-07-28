5cda8bec2849b6e6a12f33c08c0fb71f38246eb4
vkuznetsoff <ss_slavich@mail.ru>
2025-07-28 19:30:02 +0300

    init app

---
+++ src/app/page.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { Section, Cell, Image, List } from '@telegram-apps/telegram-ui';
+import { useTranslations } from 'next-intl';
+
+import { Link } from '@/components/Link/Link';
+import { LocaleSwitcher } from '@/components/LocaleSwitcher/LocaleSwitcher';
+import { Page } from '@/components/Page';
+
+import tonSvg from './_assets/ton.svg';
+
+export default function Home() {
+  const t = useTranslations('i18n');
+
+  return (
+    <Page back={false}>
+      КЛУБ ПЕРЕПРОШИТЫХ
+      <List>
+        <Section
+          header="Application Launch Data"
+          footer="These pages help developer to learn more about current launch information"
+        >
+          <Link href="/init-data">
+            <Cell subtitle="User data, chat information, technical data">
+              Init Data
+            </Cell>
+          </Link>
+          <Link href="/launch-params">
+            <Cell subtitle="Platform identifier, Mini Apps version, etc.">
+              Launch Parameters
+            </Cell>
+          </Link>
+          <Link href="/theme-params">
+            <Cell subtitle="Telegram application palette information">
+              Theme Parameters
+            </Cell>
+          </Link>
+        </Section>
+        <Section header={t('header')} footer={t('footer')}>
+          <LocaleSwitcher />
+        </Section>
+      </List>
+    </Page>
+  );
+}
