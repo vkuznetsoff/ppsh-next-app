31b56d4d0399fc932561391948238c713d285ecb
vkuznetsoff <ss_slavich@mail.ru>
2025-08-13 17:42:49 +0300

    fix: set params

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,10 @@ import {
   initDataRaw as _initDataRaw,
   initDataState as _initDataState,
   useSignal,
+ initDataQueryId as  _initDataQueryId,
+ useLaunchParams,
+ useRawLaunchParams
+ 
 } from "@telegram-apps/sdk-react";
 import { ErrorInfo, useState } from "react";
 import Error from "next/error";
@@ -39,34 +43,65 @@ export default function Home() {
 
   const initDataRaw = useSignal(_initDataRaw);
   const initDataState = useSignal(_initDataState);
+    const initDataQueryId = useSignal(_initDataQueryId);
+    const params = useRawLaunchParams()
+  console.log('initDataRaw ', initDataRaw)
+  console.log('initDataState ', initDataState)
+  console.log('initDataQueryId ', initDataQueryId)
+  console.log('useRawLaunchParams ', params)
+
 
   const register = async ():Promise<void> => {
-    const url = baseUrl + `auth/telegram/register`;
+    // const url = baseUrl + `auth/telegram/register`;
+    const url = "https://ppsh-club.ru/api/v1/auth/telegram/register"
     
-    try {
-      const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        initData: initDataRaw
-      })
-    })
+    // try {
+    //   const response = await fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json"
+    //   },
+    //   body: JSON.stringify({
+    //     initData: initDataRaw
+    //   })
+    // })
     
-     if (!response.ok) {
-      setErr("newError!!!")
-    }
+    //  if (!response.ok) {
+    //   setErr("newError!!!")
+    // }
 
-     const data: RegisterResponse = await response.json();
-     setIsRegister(true)
+    //  const data: RegisterResponse = await response.json();
+    //  setIsRegister(true)
 
-    } catch (error)  {
+    // } catch (error)  {
      
-      setErr(error as string); // Если ошибка неизвестна, установим дефолтное сообщение
-    }
+    //   setErr(error as string); // Если ошибка неизвестна, установим дефолтное сообщение
+    // }
+    fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ initData: initDataRaw }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        console.log("response.ok = ", response.ok)
+      }
+      // Возвращаем промис с типизированным JSON
+      return response.json() as Promise<RegisterResponse>;
+    })
+    .then((data) => {
+      setIsRegister(true);
+      console.log("Token:", data.token);
+      console.log("User:", data.user);
+    })
+    .catch((error: any) => {
+     console.log('ERR - ', error)
+    });
+};
     
-  }
+
 
 
   return (
