c48e8b73afb693996c5f9aa29f3e1072a60239b9
vkuznetsoff <ss_slavich@mail.ru>
2025-08-13 20:15:46 +0300

    feat: working init profile and registration 1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,15 +9,14 @@ import {
   initDataRaw as _initDataRaw,
   initDataState as _initDataState,
   useSignal,
- initDataQueryId as  _initDataQueryId,
- useLaunchParams,
- useRawLaunchParams
- 
+  initDataQueryId as _initDataQueryId,
+  //  useLaunchParams,
+  //  useRawLaunchParams
 } from "@telegram-apps/sdk-react";
 import { ErrorInfo, useState } from "react";
 import Error from "next/error";
 
-interface User {
+export interface IUser {
   telegram_id: string;
   first_name: string;
   last_name: string;
@@ -26,14 +25,15 @@ interface User {
   id: number;
 }
 
-interface RegisterResponse {
+interface IRegisterResponse {
   token: string;
-  user: User;
+  user: IUser;
 }
 
 export default function Home() {
-  const [err, setErr] = useState<String>("")
-  const [isRegister, setIsRegister] = useState<boolean>(false)
+  const [err, setErr] = useState<String>("");
+  const [isRegister, setIsRegister] = useState<boolean>(false);
+  const [user, setUser] = useState<IUser | null>(null)
 
   const t = useTranslations("i18n");
   const style = {
@@ -43,82 +43,55 @@ export default function Home() {
 
   const initDataRaw = useSignal(_initDataRaw);
   const initDataState = useSignal(_initDataState);
-    const initDataQueryId = useSignal(_initDataQueryId);
-    const params = useRawLaunchParams()
-  console.log('initDataRaw ', initDataRaw)
-  console.log('initDataState ', initDataState)
-  console.log('initDataQueryId ', initDataQueryId)
-  console.log('useRawLaunchParams ', params)
+  // const initDataQueryId = useSignal(_initDataQueryId);
+  // const params = useRawLaunchParams()
+  // console.log('initDataRaw ', initDataRaw)
+  // console.log('initDataState ', initDataState)
+  // console.log('initDataQueryId ', initDataQueryId)
+  // console.log('useRawLaunchParams ', params)
 
-
-  const register = async ():Promise<void> => {
+  const register = async (): Promise<void> => {
     // const url = baseUrl + `auth/telegram/register`;
-    const url = "https://ppsh-club.ru/api/v1/auth/telegram/register"
-    
-    // try {
-    //   const response = await fetch(url, {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json"
-    //   },
-    //   body: JSON.stringify({
-    //     initData: initDataRaw
-    //   })
-    // })
-    
-    //  if (!response.ok) {
-    //   setErr("newError!!!")
-    // }
+    const url = "https://ppsh-club.ru/api/v1/auth/telegram/register";
 
-    //  const data: RegisterResponse = await response.json();
-    //  setIsRegister(true)
-
-    // } catch (error)  {
-     
-    //   setErr(error as string); // Если ошибка неизвестна, установим дефолтное сообщение
-    // }
     fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ initData: initDataRaw }),
-  })
-    .then((response) => {
-      debugger
-      if (!response.ok) {
-        console.log("response.ok = ", response.ok)
-      }
-      // Возвращаем промис с типизированным JSON
-      return response.json() as Promise<RegisterResponse>;
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ initData: initDataRaw }),
     })
-    .then((data) => {
-      debugger
-      setIsRegister(true);
-      console.log("Token:", data.token);
-      console.log("User:", data.user);
-    })
-    .catch((error: any) => {
-      debugger
-     console.log('ERR - ', error)
-    });
-};
-    
-
-
+      .then((response) => {
+        debugger;
+        if (!response.ok) {
+          console.log("response.ok = ", response.ok);
+        }
+        // Возвращаем промис с типизированным JSON
+        return response.json() as Promise<IRegisterResponse>;
+      })
+      .then((data) => {
+        debugger;
+        setIsRegister(true);
+        console.log("Token:", data.token);
+        console.log("User:", data.user);
+      })
+      .catch((error: any) => {
+        debugger;
+        console.log("ERR - ", error);
+      });
+  };
 
   return (
     <Page back={true}>
       <div style={style}>КЛУБ ПЕРЕПРОШИТЫХ 2.0!</div>
-      <Profile initData={initDataState} register={register} />
-      <div>{initDataRaw}</div>
+      <Profile initData={initDataState} register={register} user={user}/>
       {isRegister && <div>Успех!</div>}
       {err && <div>{err}</div>}
-      <Link href="/init-data">
+      {/* <Link href="/init-data">
         <Cell subtitle="User data, chat information, technical data">
           Init Data
         </Cell>
-      </Link>
+      </Link> */}
     </Page>
   );
 }
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,34 +2,37 @@ import Image from "next/image";
 import styles from "./profile.module.scss";
 import { InitData } from "@telegram-apps/sdk-react";
 import { Button } from "@telegram-apps/telegram-ui";
+import { IUser } from "@/app/page";
 
 interface ProfileProps {
   initData: InitData | undefined;
-  register: Function 
+  register: Function;
+  user: IUser;
 }
-const Profile = ({ initData, register }: ProfileProps) => {
-    
-
-    // const handleButton = () =>  {
-    //    register()
-    // }
-
-  return (
-    <div className={styles.page}>
-      <div className={styles.header}>
-        <h2>ПРОФИЛЬ</h2>
-        <Image
-        className={styles.photo}
-          src={initData?.user?.photo_url ?? "/img/profile.jpeg"}
-          width={100}
-          height={100}
-          alt="profile"
-        />
-        <div>{initData?.user?.username}</div>
-        <Button onClick={() => register()}>Зарегистрироваться</Button>
+const Profile = ({ initData, register, user }: ProfileProps) => {
+  // const handleButton = () =>  {
+  //    register()
+  // }
+  if (true) {
+    return (
+      <div className={styles.page}>
+        <div className={styles.header}>
+          <h2>ПРОФИЛЬ</h2>
+          <Image
+            className={styles.photo}
+            src={initData?.user?.photo_url ?? "/img/profile.jpeg"}
+            width={100}
+            height={100}
+            alt="profile"
+          />
+          <div>{initData?.user?.username}</div>
+          <Button onClick={() => register()}>Зарегистрироваться</Button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    <div>Loading...</div>
+  }
 };
 
 export default Profile;
